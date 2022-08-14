c6e04ccd213c9e56f3080d4053b3d1b9ddecdef2
ElchiBey <111178447+ElchiBey@users.noreply.github.com>
2022-08-14 13:49:24 +0400

    Update LuckyPenguin.java

--- src/pgdp/luckypenguin/LuckyPenguin.java
+++ src/pgdp/luckypenguin/LuckyPenguin.java
@@ -1,43 +1,47 @@
 package pgdp.luckypenguin;
 
-import static pgdp.MiniJava.*;
+import pgdp.MiniJava;
 
-public class LuckyPenguin {
-	public static void main(String[] args) {
-      int n = readInt("Number of penguins:");
-        while (n <= 1) n = readInt("Number of penguins should be >1:");
-        int m = readInt("Starting fish per penguin:");
-        while (m <= 0) m = readInt("Starting fish should be >0:");
-        int[] f = new int[9];
-        int[] p = new int[n];
-        int[] y = new int[n];
-        int maxfish = 0;
-        for (int j = 0; j < n; j++)
-            p[j] = m;
-        int x = n;
-        while (x > 0) {
-            for (int i = 0; i < n; i++) {
-                if (p[i] == 0) x--;
-                else {
-                    writeBoard(f[0], f[1], f[2], f[3], f[4], f[5], f[6], f[7], f[8]);
+public class LuckyPenguin extends MiniJava {
+    public static void main(String[] args) {
+        int penguinNumber = readInt("Number of penguins:");
+        while (penguinNumber <= 1) penguinNumber = readInt("Number of penguins should be >1:");
+
+        int startingFishNum = readInt("Starting fish per penguin:");
+        while (startingFishNum <= 0) startingFishNum = readInt("Starting fish should be >0:");
+
+        int[] fields = new int[9];
+        int[] penguins = new int[penguinNumber];
+        int[] fishes = new int[penguinNumber];
+        int maxFish = 0;
+
+        for (int i = 0; i < penguinNumber; i++)
+            penguins[i] = startingFishNum;
+
+        int playerPengus = penguinNumber;
+
+        while (playerPengus > 0) {
+            for (int i = 0; i < penguinNumber; i++) {
+                if (penguins[i] != 0) {
+                    writeBoard(fields[0], fields[1], fields[2], fields[3], fields[4], fields[5], fields[6], fields[7], fields[8]);
                     write("It's penguin " + i + " turn:");
-                    if (x == 1) {
+                    if (playerPengus == 1) {
                         write("You are the last penguin to play! You win all the fish on the board!");
-                        p[i] += f[0] + f[1] + f[2] + f[3] + f[4] + f[5] + f[6] + f[7] + f[8];
-                        for (int j = 0; j < n; j++) {
-                            if (y[j] >= maxfish) maxfish = y[j];
+                        penguins[i] += fields[0] + fields[1] + fields[2] + fields[3] + fields[4] + fields[5] + fields[6] + fields[7] + fields[8];
+                        for (int j = 0; j < penguinNumber; j++) {
+                            if (fishes[j] >= maxFish) maxFish = fishes[j];
                         }
-                        if (maxfish <= p[i]) maxfish = p[i];
-                        if (maxfish > 1) write("The winning penguins with " + maxfish + " fishes:");
+                        if (maxFish <= penguins[i]) maxFish = penguins[i];
+                        if (maxFish > 1) write("The winning penguins with " + maxFish + " fishes:");
                         else write("The winning penguins with " + 1 + " fish:");
                         for (int t = 0; t < i; t++) {
-                            if (y[t] == maxfish) {
+                            if (fishes[t] == maxFish) {
                                 write("Penguin " + t);
                             }
                         }
-                        if (p[i] == maxfish) write("Penguin " + i);
-                        for (int t = i + 1; t < n; t++) {
-                            if (y[t] == maxfish) {
+                        if (penguins[i] == maxFish) write("Penguin " + i);
+                        for (int t = i + 1; t < penguinNumber; t++) {
+                            if (fishes[t] == maxFish) {
                                 write("Penguin " + t);
                             }
                         }
@@ -45,70 +49,70 @@ public class LuckyPenguin {
                     } else {
                         int a = dice();
                         int b = dice();
-                        int s = a + b;
-                        write(a + " + " + b + " = " + s + " was rolled.");
-                        if (s == 7) {
-                            f[4]++;
-                            p[i]--;
+                        int sum = a + b;
+                        write(a + " + " + b + " = " + sum + " was rolled.");
+                        if (sum == 7) {
+                            fields[4]++;
+                            penguins[i]--;
                             write("Wedding! You give a fish and place it on F7.");
-                        } else if (s == 2) {
-                            for(int g=0; g<9; g++){
-                                p[i] += f[g];
+                        } else if (sum == 2) {
+                            for (int g = 0; g < 9; g++) {
+                                penguins[i] += fields[g];
                             }
-                            p[i] -= f[4];
+                            penguins[i] -= fields[4];
                             write("Lucky penguin! You win all fish on the board except F7!");
-                            f[0] = 0;
-                            f[1] = 0;
-                            f[2] = 0;
-                            f[3] = 0;
-                            f[5] = 0;
-                            f[6] = 0;
-                            f[7] = 0;
-                            f[8] = 0;
-                        } else if (s == 12) {
-                            for(int g=0; g<9; g++){
-                                p[i] += f[g];
-                            }
-                            for(int g=0; g<9; g++){
-                                f[g]=0;
+                            fields[0] = 0;
+                            fields[1] = 0;
+                            fields[2] = 0;
+                            fields[3] = 0;
+                            fields[4] = 0;
+                            fields[6] = 0;
+                            fields[7] = 0;
+                            fields[8] = 0;
+                        } else if (sum == 12) {
+                            for (int g = 0; g < 9; g++) {
+                                penguins[i] += fields[g];
+                                fields[g] = 0;
                             }
                         } else {
-                            if (f[s - 3] == 0) {
-                                f[s - 3]++;
-                                p[i]--;
-                                write("You put a fish on F" + s + ".");
+                            if (fields[sum - 3] == 0) {
+                                fields[sum - 3]++;
+                                penguins[i]--;
+                                write("You put a fish on F" + sum + ".");
                             } else {
-                                f[s - 3]--;
-                                p[i]++;
-                                write("They take the fish from F" + s + ".");
+                                fields[sum - 3]--;
+                                penguins[i]++;
+                                write("They take the fish from F" + sum + ".");
                             }
                         }
-                        if (p[i] == 1) {
+
+                        if (penguins[i] == 1) {
                             write("You now have 1 fish!");
                             int exit = readInt("Enter 1 to exit now:");
                             if (exit == 1) {
-                                y[i] = p[i];
-                                p[i] = 0;
-                                x--;
+                                fishes[i] = penguins[i];
+                                penguins[i] = 0;
+                                playerPengus--;
                             }
-                        } else if (p[i] > 1) {
-                            write("You now have " + p[i] + " fishes!");
+                        } else if (penguins[i] > 1) {
+                            write("You now have " + penguins[i] + " fishes!");
                             int exit = readInt("Enter 1 to exit now:");
                             if (exit == 1) {
-                                y[i] = p[i];
-                                p[i] = 0;
-                                x--;
+                                fishes[i] = penguins[i];
+                                penguins[i] = 0;
+                                playerPengus--;
                             }
-                        } else {
-                            x--;
+                        } else if (penguins[i] == 0) {
+                            playerPengus--;
+                            penguins[i] = -1;
                             write("You now have " + 0 + " fishes!");
                             write("You have lost all fish, so you can no longer play!");
                         }
                     }
                 }
-                if (i == n-1) {
+
+                if (i == penguinNumber - 1) {
                     i = -1;
-                    x = n;
                 }
             }
         }
